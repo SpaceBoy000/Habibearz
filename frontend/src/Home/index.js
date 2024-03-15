b92a0a73bd4749ceb19fe541fac1258650da5a4f
spaceboy000 <cryptospaceboy00@gmail.com>
2024-03-15 11:50:53 +0300

    add banner image

--- frontend/src/Home/index.js
+++ frontend/src/Home/index.js
@@ -1,9 +1,18 @@
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import bannerImg from "../assets/images/banner.png";
 
 export default function Home() {
     return (
-        <div className="w-full min-h-screen p-8">
+        <div className="p-8">
+            <div className="flex">
+                <div className='text-left'>
+                    <p className="text-[#F6CE0E] uppercase">welcome to HabibearZ</p>
+                    <p>Meet HBEARZ, Not All bears are bad! Some Have Hearts.</p>
+                    <p>A Hyper Deflationary DeFi Meme coin which gives you access to the Habibears Metaverse.<br/> Come along the journey and be part of this self-sustaining ecosystem that drives its value to a sustainable model.</p>
+                </div>
+                <div>
+                    <img src={bannerImg}/>
+                </div>
+            </div>
         </div>
     );
 }
